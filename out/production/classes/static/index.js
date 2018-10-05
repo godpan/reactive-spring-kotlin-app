6b1b14aaf040f88fdb1e7d721f3ff1b4a3c4b9a9
godpan <nubug@dripower.com>
2018-10-05 18:35:49 +0800

    init

--- out/production/classes/static/index.js
+++
@@ -1,7 +0,0 @@
-var eventSource = new EventSource("/api/getStockQuotation");
-eventSource.onmessage = function(e) {
-    var li = document.createElement("li");
-    var data = JSON.parse(e.data);
-    li.innerText = "股票代码: " + data.stockQuotation.stock_id + " 股票名称:" + data.stockQuotation.stock_name + " 当前价格: " + (data.stockQuotation.price / 100.0).toFixed(2);
-    document.getElementById("stockQuotations").appendChild(li);
-}
